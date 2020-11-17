d3c82e5c0965dec1692a34cc15aaa754f3e669f0
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-11-17 02:10:54 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn Terraform Provider Versioning
+
+This repo is a companion repo to the [- Learn guide](https://learn.hashicorp.com/), containing Terraform configuration files to provision a S3 bucket.
---
+++ terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.0"
+    }
+  }
+
+  required_version = "~> 0.14"
+}
