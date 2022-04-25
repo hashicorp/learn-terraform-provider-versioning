0564464310afca73ddf39e1d950c5c2c3d639d3f
Tu Nguyen <im2nguyen@gmail.com>
2022-04-24 22:54:24 -0700

    reformat resource attributes

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ resource "random_pet" "petname" {
 
 resource "aws_s3_bucket" "sample" {
   bucket = random_pet.petname.id
-  acl    = "public-read"
 
+  acl    = "public-read"
   region = "us-west-2"
 
   tags = {
